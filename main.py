a8e6b478cdcd5b9d7d327ecc7505b6c5a429a2c8
hp <hp@DESKTOP-L7I67DA>
2021-03-22 17:21:58 +0530

    1.1 Flask

--- main.py
+++ main.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def base_route():
     return "Hello world"
 
+@app.route('/hello/<int:number>')
+def hello(number):
+    if number <= 10: return f"hello {number}", 200
+    if number >= 11: return f"hello {number}", 206
+
 if __name__=="__main__":
-    app.run()
+    app.run(debug=True)
